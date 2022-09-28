2d573b14c5bc3d3d97bdb35f8331f0d78c0a7afa
Reuben Odunze <royalrubee@gmail.com>
2022-09-29 00:21:51 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     // '~/plugins/notifier.js',
-    // '~/plugins/moralis.js',
     // '~/plugins/lazyloader.js',
     // {src: '~plugins/vue-infinite-loading', ssr: false}
   ],
